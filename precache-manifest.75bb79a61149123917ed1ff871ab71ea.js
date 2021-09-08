bdd902c5c5c83a5b882e0559ab0f57bfaa56efa0
Mickers247 <68745585+Mickers247@users.noreply.github.com>
2021-09-08 12:36:49 -0400

    Updates

---
+++ precache-manifest.75bb79a61149123917ed1ff871ab71ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a328777538e5eefabf91ff3278b3ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c110b368f53a897d809",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0e9f7e97953235aa24ec",
+    "url": "./static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "9c110b368f53a897d809",
+    "url": "./static/js/2.8dad40c2.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.8dad40c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9f7e97953235aa24ec",
+    "url": "./static/js/main.aacafb93.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
