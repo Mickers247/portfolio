4b5b84c3143d4b59100c04fa77e89ed750b793d6
Mickers247 <68745585+Mickers247@users.noreply.github.com>
2021-09-13 11:06:44 -0400

    Profile Pic

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ class About extends Component {
                 <span style={{ cursor: "auto" }}>
                   <img
                     height="250px"
-                    src="images/portfolio/animal-shelter/myProfile.jpg"
+                    src="images/portfolio/animal-shelter/myProfile3.jpg"
                     alt="Avatar placehold"
                   />
                   <Icon
